e8d6459856f2f96b2a5c97b201af6fdbf17b2858
Riccardo <riccardocracolici43@gmail.com>
2026-03-14 11:37:51 +0100

    added function to calculate basic statistics

--- src/eda_utils.py
+++ src/eda_utils.py
@@ -72,3 +72,45 @@ def plot_box(df: pd.DataFrame, column1: str, column2: str | None = None):
         # creates the box plot of column by another
         ax = sns.boxplot(data=df, x=column2, y=column1, hue=column2, palette='viridis', legend=False)
         plt.title(f"Distribution of {column1} per {column2}")
+
+
+def calculate_statistics(df: pd.DataFrame) -> pd.DataFrame:
+  """
+  Returns a Dataframe containing all descriptive statistics
+  for quantitative features.
+
+  input: pd.DataFrame
+  returns: pd.DataFrame
+  """
+  # measures of central tendency
+  mean = df.mean()
+  median = df.median()
+  mode = df.mode().iloc[0]
+  min = df.min()
+  max = df.max()
+
+  # measures of variability
+  variance = df.var()
+  std_dev = df.std()
+  range_val = df.max() - df.min()
+
+  # distribution shape
+  skewness = df.skew()
+  kurtosis = df.kurtosis()
+
+  # df containing all statistics
+  df_stats = pd.DataFrame({
+      "Mean": mean,
+      "Median": median,
+      "Mode": mode,
+      "Min": min,
+      "Max": max,
+      "Variance": variance,
+      "Standard Deviation": std_dev,
+      "Range": range_val,
+      "Skewness": skewness,
+      "Kurtosis": kurtosis
+  })
+
+  df_stats = df_stats.style.format("{:.2f}").background_gradient(cmap="viridis")
+  return df_stats
