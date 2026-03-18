4a9f6a6ff541b3d6e0bd6091c60a070b4ee1f394
Riccardo <riccardocracolici43@gmail.com>
2026-03-18 13:57:24 +0100

    removed unused libraries

--- src/models.py
+++ src/models.py
@@ -1,21 +1,13 @@
+import pandas as pd
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from catboost import CatBoostClassifier
 from sklearn.model_selection import GridSearchCV
+from src.preprocessing import get_preprocessor
 
 # for DNN
-from scikeras.wrappers import KerasClassifier
 import tensorflow as tf
 from tensorflow.keras.backend import clear_session
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import InputLayer, Dense, Dropout
-
-from src.preprocessing import get_preprocessor
 
 
 def build_dnn(n_features: int):
