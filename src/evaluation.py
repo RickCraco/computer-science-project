67bf04a64af6528e0346cc6fbc9d1e1e92cfa5c1
Riccardo <riccardocracolici43@gmail.com>
2026-03-18 17:08:56 +0100

    added required imports

---
+++ src/evaluation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, RocCurveDisplay, log_loss
+
