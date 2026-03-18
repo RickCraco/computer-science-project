92a71e4a8d30a2055ff33a03b60ce2202aaf0f4b
Riccardo <riccardocracolici43@gmail.com>
2026-03-18 13:05:16 +0100

    added function to build a DNN model

--- src/models.py
+++ src/models.py
@@ -55,3 +55,36 @@ def full_pipeline(model_name: str, numeric_cols: list, categorical_cols: list):
     ])
 
     return pipeline
+
+
+
+def build_dnn(n_features: int):
+    """
+    Build a Deep Neural Network model
+
+    The neural net has 4 hidden dense layers using
+    relu as the activation function and 1 output layer
+    for binary classification.
+
+    Input:
+    n_features: int  Number of features from X.shape[1]
+
+    Output:
+    model: obj  DNN model
+    """
+    # we use the Sequential API
+    model = Sequential()
+    model.add(InputLayer(shape=(n_features,)))  # we add the input layer
+    model.add(Dense(units=64, activation='relu'))  # first hidden layer with 64 neurons
+    model.add(Dropout(0.2))  # we add some dropout layers to prevent overfitting
+    model.add(Dense(units=64, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=32, activation='relu'))
+    model.add(Dense(units=1, activation='sigmoid'))
+
+    # we compile the model
+    model.compile(loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
