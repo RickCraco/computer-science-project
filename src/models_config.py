b2fa7eba5674eb0a382af57db0286a145146956d
Riccardo <riccardocracolici43@gmail.com>
2026-03-19 11:01:54 +0100

    imported required modules

---
+++ src/models_config.py
@@ -0,0 +1,8 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from lightgbm import LGBMClassifier
+from catboost import CatBoostClassifier
+
