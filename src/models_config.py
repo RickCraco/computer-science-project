21c057400888165b42125de8325d85d81321fce7
Riccardo <riccardocracolici43@gmail.com>
2026-03-19 12:42:41 +0100

    added more models configs

--- src/models_config.py
+++ src/models_config.py
@@ -16,7 +16,7 @@ def get_models_config():
     return [
         {
             "name": "LogisticRegression",
-            "model": LogisticRegression(),
+            "model": LogisticRegression(random_state=42),
             "param_grid": {
                 "classifier__penalty": ['l1','l2','elasticnet','none'], # regularization method
                 "classifier__C": [0.01, 0.1, 1, 10, 100],   # inverse regularization parameter, also called penalty
@@ -30,7 +30,57 @@ def get_models_config():
             "param_grid": {
                 "classifier__C": [0.01, 0.1, 1, 10],
                 "classifier__gamma": [0.001, 0.01, 0.1, 1],
-                "classifier_kernel": ['linear', 'rbf']
+                "classifier__kernel": ['linear', 'rbf']
+            }
+        },
+        {
+            "name": "DecisionTreeClassifier",
+            "model": DecisionTreeClassifier(random_state=42),
+            "param_grid": {
+                "classifier__criterion": ["gini", "entropy"],
+                "classifier__max_depth": [2, 4, 6, 8, None],
+                "classifier__min_samples_split": [2, 5, 10],
+                "classifier__min_samples_leaf": [1, 2, 4]
+            }
+        },
+        {
+            "name": "RandomForestClassifier",
+            "model": RandomForestClassifier(random_state=42),
+            "param_grid": {
+                "classifier__n_estimators": [100, 200, 300],
+                "classifier__max_depth": [2, 4, 6, 8, None],
+                "classifier__min_samples_split": [2, 5, 10],
+                "classifier__min_samples_leaf": [1, 2, 4]
+            }
+        },
+        {
+            "name": "XGBoostClassifier",
+            "model": XGBClassifier(random_state=42),
+            "param_grid": {
+                "classifier__n_estimators": [100, 200, 300],
+                "classifier__max_depth": [2, 4, 6, 8, None],
+                "classifier__learning_rate": [0.01, 0.1, 0.2],
+                "classifier__gamma": [0, 0.1, 0.2]
+            }
+        },
+        {
+            "name": "LGBMClassifier",
+            "model": LGBMClassifier(random_state=42),
+            "param_grid": {
+                "classifier__n_estimators": [100, 200, 300],
+                "classifier__learning_rate": [0.01, 0.1, 0.2],
+                "classifier__num_leaves": [15, 31, 63],
+                "classifier__max_depth": [2, 4, 6, 8, -1],
+                "classifier__reg_alpha": [0, 0.1, 0.2]
+            }
+        },
+        {
+            "name": "CatBoostClassifier",
+            "model": CatBoostClassifier(random_state=42),
+            "param_grid": {
+                "classifier__depth": [2, 4, 6, 8],
+                "classifier__learning_rate": [0.01, 0.1, 0.2],
+                "classifier__iterations": [100, 200, 300],
             }
         }
     ]
