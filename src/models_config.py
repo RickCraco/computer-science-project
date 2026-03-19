7c2c22c99f6af5f18e83b635a230731caf8c1d71
Riccardo <riccardocracolici43@gmail.com>
2026-03-19 11:29:04 +0100

    added two models configs

--- src/models_config.py
+++ src/models_config.py
@@ -6,3 +6,31 @@ from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
 
+def get_models_config():
+    """
+    Returns a list containing all the models that will
+    be tested and their param_grid for gridsearch.
+
+    Ouput: list  List of dictionaries, each dictionary is a model config
+    """
+    return [
+        {
+            "name": "LogisticRegression",
+            "model": LogisticRegression(),
+            "param_grid": {
+                "classifier__penalty": ['l1','l2','elasticnet','none'], # regularization method
+                "classifier__C": [0.01, 0.1, 1, 10, 100],   # inverse regularization parameter, also called penalty
+                "classifier__solver": ['lbfgs','newton-cg','liblinear','sag','saga'],  # solver algorithm to optimise model performance
+                "classifier__max_iter": [100, 500, 1000]  # number of iterations for model convergence 
+            }
+        },
+        {
+            "name": "SupportVectorClassifier",
+            "model": SVC(),
+            "param_grid": {
+                "classifier__C": [0.01, 0.1, 1, 10],
+                "classifier__gamma": [0.001, 0.01, 0.1, 1],
+                "classifier_kernel": ['linear', 'rbf']
+            }
+        }
+    ]
