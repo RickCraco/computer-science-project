c5ae4a298a478d7d1828cff5657ecdea2f85c726
Riccardo <riccardocracolici43@gmail.com>
2026-03-27 11:22:25 +0100

    changed param grid

--- src/models_config.py
+++ src/models_config.py
@@ -18,9 +18,9 @@ def get_models_config():
             "name": "LogisticRegression",
             "model": LogisticRegression(random_state=42),
             "param_grid": {
-                "classifier__penalty": ['l1','l2','elasticnet','none'], # regularization method
+                "classifier__penalty": ['l2',None], # regularization method
                 "classifier__C": [0.01, 0.1, 1, 10, 100],   # inverse regularization parameter, also called penalty
-                "classifier__solver": ['lbfgs','newton-cg','liblinear','sag','saga'],  # solver algorithm to optimise model performance
+                "classifier__solver": ['lbfgs','newton-cg','sag'],  # solver algorithm to optimise model performance
                 "classifier__max_iter": [100, 500, 1000]  # number of iterations for model convergence 
             }
         },
