f125594cc37d5130dd4dc0f045b2f1afc8309538
Riccardo <riccardocracolici43@gmail.com>
2026-03-28 17:43:46 +0100

    re-created function

--- src/xai.py
+++ src/xai.py
@@ -3,35 +3,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def get_feature_importance(model, preprocessor):
+def get_feature_importance(results_list: list):
     """
     Get the feature importance for a classifier
     and plots a bar chart to better visualize 
     the feature importance values.
 
     Input:
-    model:  obj  Classifier from GridSearchCV pipeline
-    preprocessor:   obj  preprocessor from GridSearchCV pipeline
+    results_list: list  list containing all trained models
     """
-    model_name = type(model.named_steps["classifier"]).__name__
-    clf = model.named_steps["classifier"]   # we retrieve the model classifier
-    feature_names = preprocessor.get_features_names_out() # we retrieve the feature names from the preprocessor 
+    # we  loop trough each model
+    for result in results_list:
+        # we retrieve the model name
+        model_name = result["model"]
 
-    # we check if the classifier has the coef_ or feature_importances attribute
-    if hasattr(clf, "coef_"):
-        importances = clf.coef_[0]
-    elif hasattr(clf, "feature_importances_"):
-        importances = clf.feature_importances_
-    else:
-        print("Model does not support feature importance")
+        # we retrieve the best estimator from GridSearchCV for both syn and real
+        model_syn = result['best_estimator_syn']
+        model_real = result['best_estimator_real']
 
-    # we save these values in a dataframe
-    feat_imp_df = pd.DataFrame({
-        "feature": feature_names,
-        "importance": importances
-    }).sort_values(by="importance", ascending=False)
-    
-    # we plot the barplot for better visualization
-    sns.barplot(data=feat_imp_df, x="importance", y="feature")
-    plt.title(f"Feature Importance - {model_name}")
-    plt.show()
+        # we retrieve the feature names
+        feature_names = model_syn.named_steps['preprocessor'].get_feature_names_out()
+
+        # we check if the model has the right attribute for feature importance
+        if hasattr(model_syn, "coef_") and hasattr(model_real, "coef_"):
+            importances_syn = model_syn.coef_[0]    # we take the feature importances for both real and syn
+            importances_real = model_real.coef_[0]
+        elif hasattr(model_syn, "feature_importances_") and hasattr(model_real, "feature_importances_"):
+            importances_syn = model_syn.feature_importances_
+            importances_real = model_real.feature_importances_
+        else:
+            print("Model does not support feature importances")
+
+        # we save the results inside a df
+        df_importance_syn = pd.DataFrame({
+            "feature": feature_names,
+            "importance_syn": importances_syn,
+        }).sort_values(by="importance_syn", ascending=False)
+
+        df_importance_real = pd.DataFrame({
+            "feature": feature_names,
+            "importance_real": importances_real
+        }).sort_values(by="importance_real", ascending=False)
+
+        # we create a subplot for multiple plots (synthetic and real)
+        fig, axs = plt.subplots(1,2, figsize=(10,8))  # we create a grid with 1 row and 2 cols
+
+        # bar chart for better visualization
+        sns.barplot(data=df_importance_syn, x="importance_syn", y="feature", ax=axs[0,0])
+        axs[0,0].set_title(f"Feature Importance - {model_name} - Synthetic")
+
+        sns.barplot(data=df_importance_real, x="importance_real", y="feature", ax=axs[0,1])
+        axs[0,1].set_title(f"Feature Importance - {model_name} - Real")
+
+        plt.tight_layout()
+        plt.show()
