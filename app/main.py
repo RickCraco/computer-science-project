02fdd742507450ad56f2916c200a9eafd8ed619b
Riccardo <riccardocracolici43@gmail.com>
2026-04-10 13:06:51 +0200

    fixed error with path

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ import gradio as gr
 import matplotlib.pyplot as plt
 import shap
 import joblib
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "..", "models", "catboost_syn.pkl")
 
 # we load the pipeline
 best_syn = joblib.load("../models/catboost_syn.pkl")
